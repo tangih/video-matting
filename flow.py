e64ce8eb4e7e809165b7d64a441da30ae118e206
tangih <tangi.hetet@polytechnique.edu>
2018-06-27 12:21:03 +0100

    flow warping

--- flow.py
+++ flow.py
@@ -1,123 +1,37 @@
 import cv2
 import numpy as np
+import progressbar
+import os
 
-import loader
+import reader
 
 
-def warp():
+def warp_img(img, flow):
+    """ warp img following optical flow (image must be 1 channel)"""
+    assert len(img.shape) == 2
+    h, w = flow.shape[:2]
+    i_, j_ = np.meshgrid(np.arange(w), np.arange(h))
+    i_ = i_.reshape((h, w, 1))
+    j_ = j_.reshape((h, w, 1))
+    identity = np.concatenate((i_, j_), axis=2)
+    map = (identity + flow).astype(np.float32)
+    return cv2.remap(img, map1=map, map2=None, interpolation=cv2.INTER_LINEAR)
+
+
+def warp_bgr(img, flow):
+    """ warp img following optical flow """
+    h, w = flow.shape[:2]
+    i_, j_ = np.meshgrid(np.arange(w), np.arange(h))
+    i_ = i_.reshape((h, w, 1))
+    j_ = j_.reshape((h, w, 1))
+    identity = np.concatenate((i_, j_), axis=2)
+    map = (identity + flow).astype(np.float32)
+    img_b = cv2.remap(img[:, :, 0], map1=map, map2=None, interpolation=cv2.INTER_LINEAR)
+    img_g = cv2.remap(img[:, :, 1], map1=map, map2=None, interpolation=cv2.INTER_LINEAR)
+    img_r = cv2.remap(img[:, :, 2], map1=map, map2=None, interpolation=cv2.INTER_LINEAR)
+    img = np.concatenate((img_b.reshape((h, w, 1)), img_g.reshape((h, w, 1)), img_r.reshape((h, w, 1))), axis=2)
+    return img
+
+
+if __name__ == '__main__':
     pass
-
-def img_flow(flow):
-    """ optical flow visualisation """
-    hsv = np.zeros((flow.shape[0], flow.shape[1], 3), dtype=np.uint8)
-    hsv[..., 1] = 255
-    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
-    hsv[..., 0] = ang * 180 / np.pi / 2
-    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    bgr = cv2.cvtColor(hsv.astype(np.uint8), cv2.COLOR_HSV2BGR)
-    return bgr
-
-
-def colorwheel(side, size):
-    h, w = size
-    center_i, center_j = int(0.75*side), int(0.75*side)
-    orig_i, orig_j = int(0.25*side), int(0.25*side)
-    flow = np.zeros((h, w, 2), dtype=np.float)
-    alpha = np.zeros((h, w), dtype=np.float)
-    for i in range(orig_i, orig_i+side):
-        for j in range(orig_j, orig_j+side):
-            s = np.linalg.norm(np.subtract([i, j], [center_i, center_j]))
-            if s <= side/2:
-                alpha[i, j] = 1.
-                if s < 0.95*side/2:
-                    flow[i, j, 0] = float(i - center_i)
-                    flow[i, j, 1] = float(j - center_j)
-    bgr = img_flow(flow)
-    return bgr, alpha
-
-
-def add_colorwheel(colorwheel, image):
-    c_bgr, c_alpha = colorwheel
-    cmp = loader.create_composite_image(c_bgr, image, c_alpha)
-    return cmp.astype(np.uint8)
-
-
-def vidflow_show(vid_img, vid_flow):
-    n_imgs = len(vid_img)
-    assert len(vid_flow) == n_imgs
-    step = 15
-    vis_list = []
-    h, w = vid_img[0].shape[:2]
-    cw = colorwheel(150, (h, w))
-    for k in range(n_imgs):
-        arrows = vid_img[i]
-        flow = vid_flow[i]
-        for i in range(0, arrows.shape[0], step):
-            for j in range(0, arrows.shape[1], step):
-                cv2.arrowedLine(arrows, (j, i), (j+int(flow[i, j, 1]), i+int(flow[i, j, 0])), color=(0, 0, 255))
-        flow_vis = img_flow(flow)
-        vis = np.concatenate((flow_vis, arrows), axis=0)
-        vis = add_colorwheel(colorwheel, vis)
-        vis_list.append(vis)
-    while True:
-        for k in range(n_imgs):
-            cv2.imshow('Optical flow visualisation', vis_list[i])
-            cv2.waitKey(30)
-
-
-
-# if __name__ == '__main__':
-#     al1, fg1 = loader.read_fg_img('./test_data/in0115.png')
-#     al2, fg2 = loader.read_fg_img('./test_data/in0119.png')
-#     # bg = cv2.imread('./test_data/sea.jpg')
-#     bg = np.zeros_like(fg1)
-#
-#     prev = loader.create_composite_image(fg1, bg, al1).astype(np.uint8)
-#     prev_gray = cv2.cvtColor(prev, cv2.COLOR_BGR2GRAY)
-#     next = loader.create_composite_image(fg2, bg, al2).astype(np.uint8)
-#     next_gray = cv2.cvtColor(next, cv2.COLOR_BGR2GRAY)
-#     # TODO: test optical flow on synthetic examples
-#     flow = cv2.calcOpticalFlowFarneback(next_gray, prev_gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-#     # swap next/prev for warp
-#     # cv2.imshow('tst', cmp1)
-#     # cv2.waitKey(0)
-#     h, w = prev.shape[:2]
-#     step = 10
-#     vis = np.mean(np.concatenate((prev.reshape(h, w, 3, 1), next.reshape(h, w, 3, 1)), axis=3),
-#                   axis=3) / 255.
-#     print(vis.shape)
-#     for i in range(0, h, step):
-#         for j in range(0, w, step):
-#             cv2.arrowedLine(vis, (j, i), (int(j+flow[i, j, 0]), int(i+flow[i, j, 1])), color=(0, 0, 255))
-#
-#     # res_b = cv2.remap(prev, map1=map_x, map2=map_y, interpolation=cv2.INTER_LINEAR)
-#     # vis = np.zeros((prev.shape[0], prev.shape[1], 3), dtype=np.float)
-#     # err = np.linalg.norm(res_b - next, axis=2) / 700.
-#     # print(np.mean(err))
-#     # cv2.imshow('res', res_b)
-#     # cv2.imshow('test', err)
-#     cv2.imshow('test', vis)
-#     cv2.waitKey(0)
-#     # cv2.imshow('test2',res)
-#     # cv2.imshow('test3',next_gray / 255.)
-#     opts = {'DIS': cv2.optflow_DISOpticalFlow,
-#             'PCA': cv2.optflow_OpticalFlowPCAFlow
-#             }
-#     for method in opts.keys():
-#         opt = opts[method]
-#         flow = np.zeros((h, w, 2), dtype=np.float)
-#         opt.calc(prev, next, flow=flow)
-#         vis = np.mean(np.concatenate((prev.reshape(h, w, 3, 1), next.reshape(h, w, 3, 1)), axis=3),
-#                       axis=3) / 255.
-#         for i in range(0, h, step):
-#             for j in range(0, w, step):
-#                 cv2.arrowedLine(vis, (j, i), (int(j + flow[i, j, 0]), int(i + flow[i, j, 1])), color=(0, 0, 255))
-#         cv2.imshow('Optical flow, {} method'.format(method), vis)
-#         cv2.waitKey(0)
-#
-#     # opt = cv2.optflow_DISOpticalFlow()
-#     # flow = np.zeros_like()
-#     # opt.calc(prev, next, flow=)
-#     # opt.
-#     # cv2.op
-#     # cv2.waitKey(0)
--- reader.py
+++ reader.py
@@ -2,6 +2,10 @@ import cv2
 import numpy as np
 import os
 
+"""
+READING
+"""
+
 
 def read_fg_img(img_path):
     """ reads a foreground RGBA image """
@@ -14,14 +18,16 @@ def read_fg_img(img_path):
     return alpha, bgr
 
 
-def create_composite_image(fg, bg, alpha):
-    """ creates composite """
-    tri_alpha = np.zeros_like(fg, dtype=np.float)
-    tri_alpha[:, :, 0] = alpha
-    tri_alpha[:, :, 1] = alpha
-    tri_alpha[:, :, 2] = alpha
-    composite = np.multiply(tri_alpha, fg) + np.multiply(1. - tri_alpha, bg)
-    return composite
+def read_flow(flow_path):
+    """ read optical flow .exr file """
+    with open(flow_path, 'rb') as f:
+        key = np.fromfile(f, dtype=np.float32, count=1)
+        if 202021.25 != key:
+            print('ERROR: invalid key ({})'.format(key))
+        w = np.fromfile(f, dtype=np.int32, count=1)[0]
+        h = np.fromfile(f, dtype=np.int32, count=1)[0]
+        data = np.fromfile(f, dtype=np.float32, count=2*h*w).reshape((h, w, 2))
+        return data
 
 
 def load_test_image(filename='in0115.png', bg_name='sea.jpg'):
@@ -37,7 +43,7 @@ def load_test_image(filename='in0115.png', bg_name='sea.jpg'):
     return fg, bg, cmp, alpha
 
 
-def load_test_video(folder_name='hairball', bg_name='grass.jpg'):
+def load_test_video(folder_name='hairball2', bg_name='grass.jpg'):
     """ loads a test video """
     print('Loading test video...')
     image_names = sorted(os.listdir(os.path.join('test_data', folder_name)))
@@ -58,15 +64,95 @@ def load_test_video(folder_name='hairball', bg_name='grass.jpg'):
     return fg_list, alpha_list, cmp_list, bg
 
 
-def vidshow(images, win_name='Video'):
+"""
+COMPOSITING
+"""
+
+
+def create_composite_image(fg, bg, alpha):
+    """ creates composite """
+    tri_alpha = np.zeros_like(fg, dtype=np.float)
+    tri_alpha[:, :, 0] = alpha
+    tri_alpha[:, :, 1] = alpha
+    tri_alpha[:, :, 2] = alpha
+    composite = np.multiply(tri_alpha, fg) + np.multiply(1. - tri_alpha, bg)
+    return composite
+
+
+def colorwheel(side, size):
+    """ create a colorwheel """
+    h, w = size
+    center_i, center_j = int(0.75*side), int(0.75*side)
+    orig_i, orig_j = int(0.25*side), int(0.25*side)
+    flow = np.zeros((h, w, 2), dtype=np.float)
+    alpha = np.zeros((h, w), dtype=np.float)
+    for i in range(orig_i, orig_i+side):
+        for j in range(orig_j, orig_j+side):
+            s = np.linalg.norm(np.subtract([i, j], [center_i, center_j]))
+            if s <= side/2:
+                alpha[i, j] = 1.
+                if s < 0.9*side/2:
+                    flow[i, j, 1] = float(i - center_i)
+                    flow[i, j, 0] = float(j - center_j)
+    bgr = img_flow(flow)
+    # cv2.imwrite('test.png', np.concatenate((bgr, (255.*alpha.reshape(h, w, 1)).astype(np.uint8)), axis=2))
+    return bgr, alpha
+
+
+def add_colorwheel(colorwheel, image):
+    """ adds the colorwheel to the image """
+    c_bgr, c_alpha = colorwheel
+    cmp = create_composite_image(c_bgr, image, c_alpha)
+    return cmp.astype(np.uint8)
+
+
+"""
+DISPLAYING
+"""
+
+
+def vidshow(images, vid_name='Video'):
     """ similar to cv2.imshow but for video """
     n_imgs = len(images)
     i = 0
     while True:
-        cv2.imshow(win_name, images[i])
+        cv2.imshow(vid_name, images[i])
         if cv2.waitKey(20) & 0xFF == ord('q'):
             break
         i = (i+1) % n_imgs
+
+
+def img_flow(flow):
+    """ optical flow visualisation """
+    hsv = np.zeros((flow.shape[0], flow.shape[1], 3), dtype=np.uint8)
+    mag, ang = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+    hsv[..., 0] = ang * 180 / np.pi / 2
+    hsv[..., 1] = 255
+    hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
+    bgr = cv2.cvtColor(hsv.astype(np.uint8), cv2.COLOR_HSV2BGR)
+    return bgr
+
+
+def vidflow_show(vid_img, vid_flow):
+    """ show flow video """
+    print('Creating visualisation')
+    n_imgs = len(vid_img)
+    assert len(vid_flow) == n_imgs
+    step = 15
+    vis_list = []
+    h, w = vid_img[0].shape[:2]
+    cw = colorwheel(150, (h, w))
+    for k in range(n_imgs):
+        arrows = vid_img[k]
+        flow = vid_flow[k]
+        for i in range(0, arrows.shape[0], step):
+            for j in range(0, arrows.shape[1], step):
+                cv2.arrowedLine(arrows, (j, i), (j+int(flow[i, j, 0]), i+int(flow[i, j, 1])), color=(0, 0, 255))
+        flow_vis = img_flow(flow)
+        flow_vis = add_colorwheel(cw, flow_vis)
+        vis = np.concatenate((flow_vis, arrows), axis=0)
+        vis_list.append(vis)
+    vidshow(vis_list, vid_name='Optical flow visualisation')
 
 
 if __name__ == '__main__':
