4e79f290da6e36c8ee17f17cd324bb096900c753
tangih <tangi.hetet@polytechnique.edu>
2018-06-27 15:52:53 +0100

    opt flow warping

--- flow.py
+++ flow.py
@@ -33,5 +33,53 @@ def warp_bgr(img, flow):
     return img
 
 
+def correct_alpha(backward, forward, alpha):
+    h, w = backward.shape[:2]
+    err = np.zeros((h, w), dtype=np.float)
+    # res = np.zeros()
+    print(forward.shape)
+    for i in range(h):
+        for j in range(w):
+            j0, i0 = backward[i, j]
+            j0, i0 = min(int(j0 + j), w-1), min(int(i0 + i), h-1)
+            # res[i, j] = img[i0, j0]
+            j1, i1 = forward[i0, j0]
+            j1, i1 = min(int(j1 + j0), w-1), min(int(i1 + i0), h-1)
+            err[i, j] = np.linalg.norm(np.array([i1, j1] - np.array([i, j])))
+    thresh = 15.
+    alpha[np.where(err > thresh)] = 0.
+    err_norm = cv2.normalize(err, None, 0., 1., cv2.NORM_MINMAX)
+    cv2.imshow('Occlusion error', err_norm)
+    # cv2.namedWindow('image')
+    # def nothing(x):
+    #     pass
+    # cv2.createTrackbar('threshold', 'image', 0, 50, nothing)
+    # while True:
+    #     excl = np.zeros((h, w), dtype=np.float)
+    #     thresh = cv2.getTrackbarPos('threshold', 'image')
+    #     excl[np.where(err > thresh)] = 1.
+    #     cv2.imshow('image', excl)
+    #     k = cv2.waitKey(1) & 0xFF
+    #     if k == 27:
+    #         break
+    return alpha
+
+
 if __name__ == '__main__':
-    pass
+    flow_f = reader.read_flow('./test_data/forward.flo')
+    flow_b = reader.read_flow('./test_data/backward.flo')
+    alp, img = reader.read_fg_img('./test_data/in0062.png')
+    # bgr = warp_bgr(img, flow_b)
+    # bgr2 = warp_bgr(bgr, flow_f)
+    h, w = img.shape[:2]
+    alpha = warp_img(alp, flow_b)
+    cv2.imshow('noncorr', alpha)
+    alpha = correct_alpha(flow_b, flow_f, alpha)
+    cv2.imshow('test', alpha)
+    cv2.waitKey(0)
+    # res = np.concatenate((bgr, (255.*alpha.reshape(img.shape[0], img.shape[1], 1)).astype(np.uint8)), axis=2) / 255.
+    # res = bgr
+    # vis = np.zeros((h, w, 3), dtype=np.float)
+    # vis[:, :, 2] = err.astype(np.float)
+    # vis = 0.5 * (vis + res)
+    # cv2.imwrite('./test_data/res.png', res)
